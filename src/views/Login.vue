c6b76ba8aad3a61e1cc8bd67c8ccbebe22d06527
hendraronaldi <hendraronaldi@hotmail.com>
2019-07-17 15:46:36 +0700

    login form UI

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -18,39 +18,20 @@
                           body-classes="px-lg-5 py-lg-5"
                           class="border-0">
                         <template>
-                            <div class="text-muted text-center mb-3">
-                                <small>Sign in with</small>
-                            </div>
-                            <div class="btn-wrapper text-center">
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/github.svg">
-                                    Github
-                                </base-button>
-
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/google.svg">
-                                    Google
-                                </base-button>
-                            </div>
-                        </template>
-                        <template>
                             <div class="text-center text-muted mb-4">
-                                <small>Or sign in with credentials</small>
+                                <small>Sign in</small>
                             </div>
                             <form role="form">
                                 <base-input alternative
                                             class="mb-3"
-                                            placeholder="Email"
-                                            addon-left-icon="ni ni-email-83">
+                                            placeholder="Username"
+                                            addon-left-icon="ni ni-single-02">
                                 </base-input>
                                 <base-input alternative
                                             type="password"
                                             placeholder="Password"
                                             addon-left-icon="ni ni-lock-circle-open">
                                 </base-input>
-                                <base-checkbox>
-                                    Remember me
-                                </base-checkbox>
                                 <div class="text-center">
                                     <base-button type="primary" class="my-4">Sign In</base-button>
                                 </div>
@@ -58,13 +39,13 @@
                         </template>
                     </card>
                     <div class="row mt-3">
-                        <div class="col-6">
+                        <!-- <div class="col-6">
                             <a href="#" class="text-light">
                                 <small>Forgot password?</small>
                             </a>
-                        </div>
+                        </div> -->
                         <div class="col-6 text-right">
-                            <a href="#" class="text-light">
+                            <a href="/#/register" class="text-light">
                                 <small>Create new account</small>
                             </a>
                         </div>
