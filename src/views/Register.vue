8d73f9793d1a2b9caf6b60c850bb007b5f01ba7b
hendraronaldi <hendraronaldi@hotmail.com>
2019-07-17 16:39:13 +0700

    register background

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,14 +1,6 @@
 <template>
     <section class="section section-shaped section-lg my-0">
-        <div class="shape shape-style-1 bg-gradient-default">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
+        <div class="shape shape-style-1 bg-default">
         </div>
         <div class="container pt-lg-md">
             <div class="row justify-content-center">
