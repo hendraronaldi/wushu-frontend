3981ef088d981523f8a9e54e852788febfd538a0
hendraronaldi <hendraronaldi@hotmail.com>
2019-07-21 23:00:14 +0700

    admin login, logout, get users, validate, reject

--- src/store/actions.js
+++ src/store/actions.js
@@ -11,6 +11,16 @@ export const login = async ({commit}, payload) => {
     }
 }
 
+export const adminLogin = async ({commit}, payload) => {
+    try {
+        const response = await axios.post(BASE_URL + "/admin-login", payload.user)
+        commit('adminLogin', response.data)
+        return Promise.resolve(response.data)
+    } catch (e) {
+        return Promise.reject(e)
+    }
+}
+
 export const register = async ({commit}, payload) => {
     try {
         const response = await axios.post(BASE_URL + "/register", payload.user)
@@ -31,4 +41,53 @@ export const logout = ({commit}, payload) => {
     // }
     commit('logout')
     return true
+}
+
+export const adminLogout = ({commit}, payload) => {
+    // try {
+    //     const response = await axios.post(BASE_URL + "/logout", payload.user)
+    //     commit('register', response.data.response)
+    //     return Promise.resolve(response.data.response)
+    // } catch (e) {
+    //     return Promise.reject(e)
+    // }
+    commit('adminLogout')
+    return true
+}
+
+export const editUser = async ({commit}, payload) => {
+    try {
+        const response = await axios.put(BASE_URL + "/edituser", payload.user)
+        commit('editUser', response.data)
+        return Promise.resolve(response.data)
+    } catch (e) {
+        return Promise.reject(e)
+    }
+}
+
+export const getAllUsers = async ({commit}) => {
+    try {
+        const response = await axios.get(BASE_URL + "/users")
+        return Promise.resolve(response.data)
+    } catch (e) {
+        return Promise.reject(e)
+    }
+}
+
+export const validateUser = async ({commit}, payload) => {
+    try {
+        const response = await axios.post(BASE_URL + "/validate", payload.user)
+        return Promise.resolve(response.data)
+    } catch (e) {
+        return Promise.reject(e)
+    }
+}
+
+export const rejectUser = async ({commit}, payload) => {
+    try {
+        const response = await axios.post(BASE_URL + "/reject", payload.user)
+        return Promise.resolve(response.data)
+    } catch (e) {
+        return Promise.reject(e)
+    }
 }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 
 export default {
+    editUser(state, user) {
+        editUser(state, user);
+    },
+
     login(state, user) {
         login(state, user);
     },
@@ -11,20 +15,43 @@ export default {
 
     logout(state, user) {
         logout(state, user);
+    },
+
+    adminLogin(state, user) {
+        adminLogin(state, user);
+    },
+
+    adminLogout(state, user) {
+        adminLogout(state, user);
     }
 }
 
-function login(state, user) {
-    Vue.set(state, 'userLogged', true);
+function editUser(state, user) {
     Vue.set(state, 'userProfile', user);
+    localStorage.setItem('userProfile', JSON.stringify(user));
+}
+
+function login(state, user) {
+    Vue.set(state, 'userProfile', user);
+    localStorage.setItem('userProfile', JSON.stringify(user));
 }
 
 function register(state, user) {
-    Vue.set(state, 'userLogged', true);
     Vue.set(state, 'userProfile', user);
+    localStorage.setItem('userProfile', JSON.stringify(user));
 }
 
 function logout(state) {
-    Vue.set(state, 'userLogged', false);
     Vue.set(state, 'userProfile', null);
+    localStorage.setItem('userProfile', null);
+}
+
+function adminLogin(state, user) {
+    Vue.set(state, 'admin', user);
+    localStorage.setItem('admin', JSON.stringify(user));
+} 
+
+function adminLogout(state) {
+    Vue.set(state, 'admin', null);
+    localStorage.setItem('admin', null);
 }
